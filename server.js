0cb63d6e3f5685c0896ff024970b7805a669ef38
Dave Arroyo <arroyo.daveil@gmail.com>
2017-05-07 11:25:51 +0800

    Add Procfile

--- server.js
+++ server.js
@@ -16,10 +16,21 @@ const wss = new SocketServer({ server });
 wss.on('connection', (ws) => {
   console.log('Client connected');
   ws.on('close', () => console.log('Client disconnected'));
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+    wss.clients.forEach((client) => {
+    	 console.log('client: %s', client);
+    	var messageObj = {message:message};
+	    client.send(JSON.stringify(messageObj));
+	  });
+  });
 });
+
+
 
 setInterval(() => {
   wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
+  	var timeObj  ={date:new Date().toTimeString()};
+    client.send(JSON.stringify(timeObj));
   });
 }, 1000);
